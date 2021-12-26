d92a59b9be6f2e9bf7a2ceee743bf9d0656dfdb9
Francis Zhang <francislovem@gmail.com>
2021-12-27 00:42:39 +0800

    add human input

---
+++ src/human.go
@@ -0,0 +1,107 @@
+package src
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func createHumanReactFunc(selfIndex int) func(*Board) Action {
+	return func(board *Board) Action {
+		if board == nil || selfIndex < 0 || len(board.Players) <= selfIndex || board.Game == nil {
+			panic("humanReact invalid inputs")
+		}
+
+		minRequiredAmount := board.Game.CurrentAmount - board.Players[selfIndex].InPotAmount
+		bankroll := board.Players[selfIndex].Bankroll
+
+		var desc string
+		if bankroll <= minRequiredAmount {
+			desc = "You can choose (enter number): \n" +
+				"[!] Bet\n" +
+				"[!] Call\n" +
+				"[3] Fold\n" +
+				"[4] AllIn\n"
+		} else {
+			desc = "You can choose (enter number): \n" +
+				"[1] Bet\n" +
+				"[2] Call\n" +
+				"[3] Fold\n" +
+				"[4] AllIn\n"
+		}
+
+		wrongInputCount := 0
+		wrongInputLimit := 3
+		for wrongInputCount < wrongInputLimit {
+			fmt.Print(desc)
+			reader := bufio.NewReader(os.Stdin)
+			actionNumber, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Printf("input error: %v\n", err)
+				wrongInputCount++
+				continue
+			}
+
+			if actionNumber == "1" {
+				if bankroll <= minRequiredAmount {
+					wrongInputCount++
+					continue
+				}
+
+				fmt.Printf("How much do you want to bet? [%d, %d]\n", minRequiredAmount, bankroll)
+				reader := bufio.NewReader(os.Stdin)
+				amountStr, err := reader.ReadString('\n')
+				if err != nil {
+					fmt.Printf("input error: %v\n", err)
+					wrongInputCount++
+					continue
+				}
+
+				amount, err := strconv.Atoi(amountStr)
+				if err != nil {
+					fmt.Printf("Atoi error: %v\n", err)
+					wrongInputCount++
+					continue
+				} else if amount < minRequiredAmount || amount > bankroll {
+					fmt.Println("invalid input amount")
+					wrongInputCount++
+					continue
+				}
+
+				return Action{
+					ActionType: ActionTypeBet,
+					Amount:     amount,
+				}
+
+			} else if actionNumber == "2" {
+				if bankroll <= minRequiredAmount {
+					wrongInputCount++
+					continue
+				}
+				return Action{
+					ActionType: ActionTypeCall,
+					Amount:     minRequiredAmount,
+				}
+
+			} else if actionNumber == "3" {
+				return Action{
+					ActionType: ActionTypeFold,
+					Amount:     0,
+				}
+
+			} else if actionNumber == "4" {
+				return Action{
+					ActionType: ActionTypeAllIn,
+					Amount:     bankroll,
+				}
+
+			} else {
+				wrongInputCount++
+				continue
+			}
+		}
+
+		return Action{ActionType: ActionTypeFold, Amount: 0}
+	}
+}
