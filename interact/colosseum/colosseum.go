a350ed4a47f3468b763c8730b090533b484bc828
FrancisZhang <francislovem@gmail.com>
2022-01-03 04:13:39 +0800

    prepare to refactor

---
+++ interact/colosseum/colosseum.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"holdem/constant"
+	"holdem/process"
+	"os"
+)
+
+func main() {
+	constant.DebugMode = false
+	constant.Language = constant.ZH_CN
+
+	playerNum := 6
+	playerBankroll := 100
+	smallBlinds := 1
+	board := process.InitBoard(playerNum, playerBankroll)
+
+	for cycle := 0; cycle < 2; cycle++ {
+		for match := 0; match < playerNum; match++ {
+			process.InitGame(board, smallBlinds, match, fmt.Sprintf("cycle%d_match%d", cycle+1, match+1))
+			process.PlayGame(board)
+			process.EndGame(board)
+
+			fmt.Printf("Match finish. Press any key to begin next match.\n")
+			reader := bufio.NewReader(os.Stdin)
+			reader.ReadString('\n')
+		}
+	}
+
+	fmt.Printf("Game Over. Press any key to exit.\n")
+	reader := bufio.NewReader(os.Stdin)
+	reader.ReadString('\n')
+}
